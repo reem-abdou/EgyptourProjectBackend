acd8e9dd7a4a4d52e540e1a4099e0d53d6982a54
reem-abdou <146747432+reem-abdou@users.noreply.github.com>
2025-05-13 21:19:26 +0300

    Create DTOS

---
+++ EgyptourProject/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EgyptourProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        //private readonly IUserManager _userManager;
+        //public AdminController(IAdminManager userManager)
+        //{
+        //    _userManager = userManager;
+
+        //}
+    }
+}
